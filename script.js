642096fda9a2f5e7ca6063b4392a891e3cdcc6db
Peter Yang <pyangmain@gmail.com>
2022-07-11 18:39:51 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const startButton = document.getElementById('start-button')
+
+startButton.addEventListener('click', startGame)
+
+function startGame() {
+    console.log('hello')
+    //const lowerBound = document.getElementById('lower-bound').value
+    //const upperBound = document.getElementById('upper-bound').value
+    //console.log(lowerBound)   
+    //console.log(upperBound)
+}
